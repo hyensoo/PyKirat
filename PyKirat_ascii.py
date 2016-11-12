996a4fba8778b51844033c606fe452d9af3cb889
hyensoo <hyensoochoi@gmail.com>
2016-11-12 10:38:36 +0900

    First commit

---
+++ PyKirat_ascii.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+data=list(raw_input("Enter PlainText: "))
+binary=[]
+for i in data:
+    binary.append(bin(ord(i)))
+for j in binary:
+    for k in list(j[2:]):
+        if k=='1':
+            print "키랏",
+        else:
+            print "★",
